975e006d63eb36f6969e91f02fd79d01f50fdd5a
Kai Naschinski <kai@naschinski.de>
2019-11-01 14:56:03 +0100

    Run only coff related tests

--- lld/run.sh
+++ lld/run.sh
@@ -1,9 +1,9 @@
 set -e
 cd /home/kai/master/llvm-project/out/release
-if [ "$1" = "test" ]; then
-   ninja check-lld
-fi
 ninja lld
+if [ "$1" = "test" ]; then
+  /home/kai/master/llvm-project/out/release/bin/llvm-lit /home/kai/master/llvm-project/lld/test/COFF -s -vv
+fi
 cd /home/kai/master/test
 if [ "$1" = "debug" ]; then
    ninja debug
