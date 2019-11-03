b863bdfa5c942c28c1d4f02ab39c5df033da21bc
Kai Naschinski <kai@naschinski.de>
2019-11-03 22:14:57 +0100

    Update run script

--- lld/run.sh
+++ lld/run.sh
@@ -1,12 +1,16 @@
 set -e
-cd /home/kai/master/llvm-project/out/release
+if [ "$1" = "debug" ]; then
+  cd /home/kai/master/llvm-project/out/debug
+else
+  cd /home/kai/master/llvm-project/out/release
+fi
 ninja lld
 if [ "$1" = "test" ]; then
   /home/kai/master/llvm-project/out/release/bin/llvm-lit /home/kai/master/llvm-project/lld/test/COFF -s -vv
 fi
 cd /home/kai/master/test
 if [ "$1" = "debug" ]; then
-   ninja debug
+  ninja debug
 fi
 ninja
 wine a.exe
